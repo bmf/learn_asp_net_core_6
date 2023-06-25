4660dff4856723c52fa6810bc1077d4b202c6d72
_BMF_ <brandon.flade@gmail.com>
2023-06-25 17:12:16 -0400

    Added calls to save data to SQL Database

--- RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Checkout
 {
     [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        private readonly ApplicationDBContext _context;
+        public CheckoutModel(ApplicationDBContext context)
+        {
+            _context = context;
+        }
 
         public string PizzaName { get; set; }
         public float PizzaPrice { get; set; }
@@ -20,6 +27,16 @@ namespace RazorPizzeria.Pages.Checkout
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+            
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
+
         }
+
+       
     }
 }
